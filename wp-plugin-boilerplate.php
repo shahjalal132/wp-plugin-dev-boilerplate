7763cc73a581dacb37ea35273a31f899ee78b72a
mtmsujon <mtmsujon@gmail.com>
2024-08-07 10:26:35 +0600

    initial plugin bootstrap

---
+++ wp-plugin-boilerplate.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * 
+ * The plugin bootstrap file
+ * 
+ * @link              https://github.com/mtmsujan
+ * @since             1.0.0
+ * @package           wp-plugin-boilerplate
+ * 
+ * Plugin Name: WP Plugin Boilerplate
+ * Plugin URI:  #
+ * Author:      MTM Sujon
+ * Author URI:  https://github.com/mtmsujan
+ * Description: WordPress Plugin Boilerplate.
+ * Version:     1.0.0
+ * License:     GPL-2.0+
+ * License URL: http://www.gnu.org/licenses/gpl-2.0.txt
+ * text-domain: wp-plugin-boilerplate
+ */
