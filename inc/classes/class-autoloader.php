6e54ff128a85ec3440d6d5957efe546b92a35442
mtmsujon <mtmsujon@gmail.com>
2024-08-11 15:21:49 +0600

    Added class-autoloader.php file

---
+++ inc/classes/class-autoloader.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Bootstraps the plugin.
+ */
+
+namespace BOILERPLATE\Inc;
+
+use BOILERPLATE\Inc\Traits\Singleton;
+
+class Autoloader {
+    use Singleton;
+
+    protected function __construct() {
+
+        // load class.
+
+    }
+}
