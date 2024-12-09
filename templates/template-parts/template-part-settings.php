888ed7dcc7ec6e9b3cab04c6643afeab49867503
shahjalal <rjshahjalal132@gmail.com>
2024-12-09 12:10:05 +0600

    added setting1 button

--- templates/template-parts/template-part-settings.php
+++ templates/template-parts/template-part-settings.php
@@ -1,1 +1,6 @@
 <h4 class="common-title">Settings</h4>
+
+<button type="button" class="common-btn mt-20 button-flex" id="setting1">
+    <span>Setting 1</span>
+    <span class="setting1-spinner-loader-wrapper"></span>
+</button>
