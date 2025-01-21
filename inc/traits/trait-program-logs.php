6b23fb86dde8969b6d180497f4e44a9c65640d62
Muhammad Shah Jalal <128796759+shahjalal132@users.noreply.github.com>
2025-01-21 12:05:56 +0600

    Update trait-program-logs.php

--- inc/traits/trait-program-logs.php
+++ inc/traits/trait-program-logs.php
@@ -15,25 +15,22 @@ trait Program_Logs {
     }
 
     final public function put_program_logs( $data ) {
+     // Ensure the directory for logs exists
+    $directory = PLUGIN_BASE_PATH . '/program_logs/';
+    if ( !file_exists( $directory ) ) {
+        mkdir( $directory, 0777, true );
+    }
 
-        // Ensure the directory for logs exists
-        $directory = PLUGIN_BASE_PATH . '/program_logs/';
-        if ( !file_exists( $directory ) ) {
-            mkdir( $directory, 0777, true );
-        }
+    // Construct the log file path
+    $file_name     = $directory . 'program_logs.log';
+    $file_name_txt = $directory . 'program_logs.txt';
 
-        // Construct the log file path
-        $file_name = $directory . 'program_logs.log';
+    // Append the current datetime to the log entry
+    $current_datetime = date( 'Y-m-d H:i:s' );
+    $data             = $data . ' - ' . $current_datetime;
 
-        // Append the current datetime to the log entry
-        $current_datetime = date( 'Y-m-d H:i:s' );
-        $data             = $data . ' - ' . $current_datetime;
-
-        // Write the log entry to the file
-        if ( file_put_contents( $file_name, $data . "\n\n", FILE_APPEND | LOCK_EX ) !== false ) {
-            return "Data appended to file successfully.";
-        } else {
-            return "Failed to append data to file.";
-        }
+    // Write the log entry to the file
+    file_put_contents( $file_name, $data . "\n\n", FILE_APPEND | LOCK_EX );
+    file_put_contents( $file_name_txt, $data . "\n\n", FILE_APPEND | LOCK_EX );
     }
 }
